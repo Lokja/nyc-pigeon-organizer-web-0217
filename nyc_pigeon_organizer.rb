67506cc1cb187c8311429b3f4a6a088897ca6578
Devin Rieger <dev0089@gmail.com>
2017-02-04 21:26:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,51 @@
+require "pry"
+
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeon_list = {}
+  names = []
+  pigkeys = data.keys
+
+  data.each do |cgl, value|
+    value.each do |k, v|
+      names << v
+    end
+  end
+
+  names = names.flatten.uniq
+
+  names.each do |name|
+    pigeon_list[name] = {}
+    pigkeys.each do |key|
+      pigeon_list[name][key] = []
+    end
+  end
+
+  data.each do |cgl, value|
+    value.each do |k, v|
+      v.each do |name|
+        pigeon_list[name][cgl] << k.to_s
+      end
+    end
+  end
+
 end
